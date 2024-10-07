25184eb106fa404d3240a8a36184e9dab8a80e34
DRK DEVELOPER <146550095+DRKALPHA@users.noreply.github.com>
2024-10-07 09:21:30 +0530

    Add files via upload

---
+++ plugins/support.js
@@ -0,0 +1,47 @@
+const config = require('../config')
+
+const {cmd , commands} = require('../command')
+
+
+
+cmd({
+
+    pattern: "support",
+
+    desc: "To get the bot informations.",
+
+    react: "⛓",
+
+    category: "main",
+
+    filename: __filename
+
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+
+try{
+
+
+
+let about = ` *👋 Hello ${pushname}*
+
+*🧚QUEEN-KYLIE-MD Support Channels🧚*
+
+*Youtube Channel Link:* https://www.youtube.com/@Sahas_Tech
+
+*Whatsapp Channel Link:* https://whatsapp.com/channel/0029VaiTjMlK5cDLek3bB533
+
+*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`
+
+return await conn.sendMessage(from,{image: {url:`https://i.ibb.co/XLbkDd4/IMG-20240913-WA0048.jpg`},caption:about},{quoted: mek})
+
+}catch(e){
+
+console.log(e)
+
+reply(`${e}`)
+
+}
+
+})
